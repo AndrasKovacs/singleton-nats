a32c6537bba4dfde0a03780bcbe8d5bc889698f9
András Kovács <puttamalac@gmail.com>
2015-05-06 10:17:19 +0200

    add some docs, export natMul and natAdd instead of (+) and (*)

--- Data/Nat.hs
+++ Data/Nat.hs
@@ -5,8 +5,8 @@
 
 module Data.Nat (
     Nat(..)
-  , (Data.Nat.+)
-  , (Data.Nat.*)
+  , natPlus
+  , natMul
   , SNat
   , Data.Singletons.Prelude.Sing(..)
   , (:*)
@@ -38,12 +38,25 @@ $(singletons [d|
   Z * b = Z
   S a * b = b + (a * b) |])
 
+{-| This is the plain value-level version of addition on Nats. There's rarely a reason to use this;
+it's included for completeness. -}
+natPlus :: Nat -> Nat -> Nat
+natPlus = (Data.Nat.+)
+
+{-| Similarly to 'natPlus', this one is included for completeness. -}
+natMul :: Nat -> Nat -> Nat
+natMul = (Data.Nat.*)
+
+{-| Provides a shorthand for 'Nat'-s using "GHC.TypeLits", for example:
+
+>>> :kind! Lit 3
+Lit 3 :: Nat
+= 'S ('S ('S 'Z))
+-}
+
 type family Lit n where
   Lit 0 = Z
   Lit n = S (Lit (n Lit.- 1))
 
 type SLit n = Sing (Lit n)
-
-
-
 
